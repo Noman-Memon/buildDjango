c6a8b251668fe8805c3a5547e27b8cb1348b23f0
Noman-Memon <96913980+Noman-Memon@users.noreply.github.com>
2023-01-04 23:43:52 +0500

    create models and make migrations

--- chefsTable/myapp/models.py
+++ chefsTable/myapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class MenuItems(models.Model):
+    itemName = models.CharField(max_length=200)
+    categorey = models.CharField(max_length=300)
+    year = models.IntegerField()
+
+class Drinks(models.Model):
+    drink_name = models.CharField(max_length=200)
+    price = models.IntegerField()
